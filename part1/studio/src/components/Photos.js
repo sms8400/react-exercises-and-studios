bc566afe855dca4c69d84d3c7f733a7c980fb3e5
Sue <138933788+sms8400@users.noreply.github.com>
2023-08-07 16:13:14 -0400

    commited

--- part1/studio/src/components/Photos.js
+++ part1/studio/src/components/Photos.js
@@ -1,7 +1,7 @@
-import styles from './Description.module.css';
+import styles from './Description.module.css;
 
 export default function RecipePhoto() {
   return(
-    <img src =  "https://altonbrown.com/wp-content/uploads/2020/08/Basic-Crepes-1024x642.jpeg" width = "400" alt = "crepes" className = {StyleSheet.imageUpdates} />
+    <img src =  "https://altonbrown.com/wp-content/uploads/2020/08/Basic-Crepes-1024x642.jpeg" width = "auto" alt = "crepes" className = {StyleSheet.imageUpdates} />
   );
 }
