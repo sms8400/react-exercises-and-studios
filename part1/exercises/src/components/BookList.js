cc59c69c2025c740884897bc1aa392e057c24811
Sue <138933788+sms8400@users.noreply.github.com>
2023-08-07 14:01:43 -0400

    save

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -6,10 +6,10 @@ export default function BookList() {
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" />
+      <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" />
+      <img src={book3} alt="The London Seance Society by Sarah Penner" />
+      </div>    
    );
 }
